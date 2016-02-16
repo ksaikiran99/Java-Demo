14fdfeb5bfbe7f0fe150a80ba46e4086ee2c1e71
ksaikiran99 <ksaikiran99@gmail.com>
2016-02-16 01:26:20 -0700

    Addition has been added

--- src/demo.java
+++ src/demo.java
@@ -3,8 +3,12 @@ public class demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Mission");
-
+		System.out.println("Mission Impossible");
+		System.out.println(addition(6));
 	}
-
+	public static int addition(int c)
+	{
+		return (c+2)/2;
+		
+	}
 }
